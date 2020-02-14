a3613a80891daf8d2feb0e1085069141c19d9947
dacodingguy <c22cg@dalton.org>
2020-02-14 15:39:40 -0500

    Added Turn360 OpMode as test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Turn360.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name = "Turn360", group = "FoundationAuto")
+
+public class Turn360 extends LinearOpMode {
+
+
+    private ElapsedTime runtime = new ElapsedTime();
+    MecanumRobot rb = new MecanumRobot();
+
+    public static void resetEncoder(DcMotor motor) {
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initializing");
+
+        rb.init(hardwareMap, this);
+
+        rb.flMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rb.frMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rb.blMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rb.brMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        resetEncoder(rb.frMotor);
+        resetEncoder(rb.flMotor);
+        resetEncoder(rb.blMotor);
+        resetEncoder(rb.brMotor);
+
+        rb.hooksUp();
+
+        telemetry.addData("Status", "Initialized");
+
+        telemetry.update();
+
+        Thread.sleep(1000);
+        waitForStart();
+        rb.turnClockwiseByEncoder(2000,rb.flMotor,.4);
+        runtime.reset();
+    }
+}
+
